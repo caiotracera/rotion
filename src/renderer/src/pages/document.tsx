65b0ee05798a734432498a2230a323316bbfc674
Caio Tracera <ctrcra@gmail.com>
2023-05-29 14:53:34 -0300

    feat(pages): add document page

--- src/renderer/src/pages/document.tsx
+++ src/renderer/src/pages/document.tsx
@@ -1,7 +1,22 @@
+import { ToC } from '../components/ToC';
+
 export function Document() {
   return (
-    <main className="flex-1 flex items-center justify-center text-rotion-400">
-      Document
+    <main className="flex-1 flex py-12 px-10 gap-8">
+      <aside className="hidden lg:block sticky top-0">
+        <span className="text-rotion-300 font-semibold uppercase text-xs">
+          TABLE OF CONTENTS
+        </span>
+
+        <ToC.Root>
+          <ToC.Link>Arquitetura Back-end</ToC.Link>
+          <ToC.Section>Banco de Dados</ToC.Section>
+          <ToC.Section>Autenticação</ToC.Section>
+          <ToC.Section>Mensageria</ToC.Section>
+        </ToC.Root>
+      </aside>
+
+      <section className="flex-1 flex flex-col items-center"></section>
     </main>
   );
 }
