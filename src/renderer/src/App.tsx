45fd3cd3d739a8f5f0cbec1665fac82ba5267d5c
Caio Tracera <ctrcra@gmail.com>
2023-05-29 08:33:42 -0300

    feat(deps): configure tailwindcss

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,9 @@
+import './styles/global.css';
+
 export function App(): JSX.Element {
   return (
     <div>
-      <h1>hello world</h1>
+      <h1 className="text-violet-400 font-bold text-4xl">hello world</h1>
     </div>
   );
 }
