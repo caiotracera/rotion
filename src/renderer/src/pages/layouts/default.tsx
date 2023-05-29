b75af0be77d61cadeb310a93bb51d3ce9270a867
Caio Tracera <ctrcra@gmail.com>
2023-05-29 15:28:05 -0300

    feat(sidebar): add collapsble sidebar

--- src/renderer/src/pages/layouts/default.tsx
+++ src/renderer/src/pages/layouts/default.tsx
@@ -1,17 +1,29 @@
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import * as Collapsible from '@radix-ui/react-collapsible';
 
 import { Sidebar } from '../../components/Sidebar';
 import { Header } from '../../components/Header';
 
 export function Default() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  function handleToggleSidebar() {
+    setIsSidebarOpen((prev) => !prev);
+  }
+
   return (
-    <div className="h-screen w-screen bg-rotion-900 text-rotion-100 flex">
+    <Collapsible.Root
+      defaultOpen
+      onOpenChange={handleToggleSidebar}
+      className="h-screen w-screen bg-rotion-900 text-rotion-100 flex"
+    >
       <Sidebar />
       <div className="flex-1 flex flex-col max-h-screen">
-        <Header />
+        <Header isSidebarOpen={isSidebarOpen} />
 
         <Outlet />
       </div>
-    </div>
+    </Collapsible.Root>
   );
 }
